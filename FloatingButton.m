ae95f3df1e63e33f16f9712be9f8eed1f1a8d7b4
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-23 16:38:32 +0300

    Update FloatingButton.m

--- FloatingButton.m
+++ FloatingButton.m
@@ -19,13 +19,12 @@
     return self;
 }
 
-- (void)move:(UIPanGestureRecognizer *)pan {
-
+- (void)move:(UIPanGestureRecognizer *)p {
     UIView *v = self.superview;
-    CGPoint t = [pan translationInView:v];
+    CGPoint t = [p translationInView:v];
 
     self.center = CGPointMake(self.center.x + t.x, self.center.y + t.y);
-    [pan setTranslation:CGPointZero inView:v];
+    [p setTranslation:CGPointZero inView:v];
 }
 
 @end
