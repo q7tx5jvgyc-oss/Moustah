3b1d4cd3af65eee504ec8afb9d668a5c46203fc0
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-23 20:33:34 +0300

    Update LicenseManager.m

--- LicenseManager.m
+++ LicenseManager.m
@@ -11,15 +11,67 @@
     return m;
 }
 
-- (BOOL)isActivated {
-    return [[NSUserDefaults standardUserDefaults] boolForKey:@"activated"];
+// قائمة الأكواد (20 كود)
+- (NSArray *)validCodes {
+    return @[
+        @"MOSTAH-001",
+        @"MOSTAH-002",
+        @"MOSTAH-003",
+        @"MOSTAH-004",
+        @"MOSTAH-005",
+        @"MOSTAH-006",
+        @"MOSTAH-007",
+        @"MOSTAH-008",
+        @"MOSTAH-009",
+        @"MOSTAH-010",
+        @"MOSTAH-011",
+        @"MOSTAH-012",
+        @"MOSTAH-013",
+        @"MOSTAH-014",
+        @"MOSTAH-015",
+        @"MOSTAH-016",
+        @"MOSTAH-017",
+        @"MOSTAH-018",
+        @"MOSTAH-019",
+        @"MOSTAH-020"
+    ];
 }
 
+// جلب معرف الجهاز
+- (NSString *)deviceID {
+    NSString *idfv = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+    return idfv ?: @"unknown_device";
+}
+
+// تحقق من التفعيل
 - (BOOL)validateCode:(NSString *)code {
-    if ([code isEqualToString:@"MOSTAH776776"]) {
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"activated"];
+
+    NSUserDefaults *def = [NSUserDefaults standardUserDefaults];
+
+    // إذا الجهاز مفعّل مسبقًا
+    if ([def boolForKey:@"activated"]) {
         return YES;
     }
+
+    // تحقق من الكود
+    if ([[self validCodes] containsObject:code]) {
+
+        // تحقق هل الكود مستخدم قبل
+        NSString *usedKey = [NSString stringWithFormat:@"used_%@", code];
+        if ([def boolForKey:usedKey]) {
+            return NO; // مستخدم قبل
+        }
+
+        // حفظ التفعيل
+        [def setBool:YES forKey:@"activated"];
+        [def setObject:[self deviceID] forKey:@"device_id"];
+        [def setBool:YES forKey:usedKey];
+
+        [def synchronize];
+
+        return YES;
+    }
+
     return NO;
 }
 
