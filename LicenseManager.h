5833ebe21528f93e466ebd5e35369559bd3df301
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-28 01:27:42 +0300

    Update LicenseManager.h

--- LicenseManager.h
+++ LicenseManager.h
@@ -1,7 +1,16 @@
 #import <Foundation/Foundation.h>
 
+typedef NS_ENUM(NSInteger, LicenseType) {
+    LicenseTypeInvalid,
+    LicenseTypeDaily,
+    LicenseTypePermanent
+};
+
 @interface LicenseManager : NSObject
+
 + (instancetype)shared;
+
+- (LicenseType)validateCode:(NSString *)code;
 - (BOOL)isActivated;
-- (BOOL)validateCode:(NSString *)code;
+
 @end
