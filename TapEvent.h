1e8651b14209665149401f93608560c155fece13
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-29 00:03:46 +0300

    Delete TapEvent.h

--- TapEvent.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-@interface TapEvent : NSObject <NSSecureCoding>
-
-@property (nonatomic, assign) CGPoint position;
-@property (nonatomic, assign) NSTimeInterval time;
-
-@end
