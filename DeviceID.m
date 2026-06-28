bea06006e67f11a4b9178406bc184d8985b52f0e
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-28 14:53:54 +0300

    Update DeviceID.m

--- DeviceID.m
+++ DeviceID.m
@@ -1,25 +1,74 @@
 #import "DeviceID.h"
 #import <UIKit/UIKit.h>
 #import <CommonCrypto/CommonCrypto.h>
+#import <Security/Security.h>
 
 @implementation DeviceID
 
-+ (NSString *)generate {
++ (NSString *)getOrCreateKeychainID {
 
-    NSString *base = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
+    NSString *service = @"com.mostah.deviceid";
+    NSString *account = @"device";
 
-    const char *cStr = [base UTF8String];
-    unsigned char digest[CC_SHA256_DIGEST_LENGTH];
+    NSDictionary *query = @{
+        (__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
+        (__bridge id)kSecAttrService: service,
+        (__bridge id)kSecAttrAccount: account,
+        (__bridge id)kSecReturnData: @YES
+    };
 
-    CC_SHA256(cStr, (CC_LONG)strlen(cStr), digest);
+    CFTypeRef result = NULL;
+    SecItemCopyMatching((__bridge CFDictionaryRef)query, &result);
 
-    NSMutableString *out = [NSMutableString string];
-
-    for (int i = 0; i < 10; i++) {
-        [out appendFormat:@"%02x", digest[i]];
+    if (result) {
+        NSData *data = (__bridge_transfer NSData *)result;
+        return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
     }
 
-    return out;
+    // إنشاء ID جديد
+    NSString *newID = [self createRawDeviceID];
+    NSData *data = [newID dataUsingEncoding:NSUTF8StringEncoding];
+
+    NSDictionary *addQuery = @{
+        (__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
+        (__bridge id)kSecAttrService: service,
+        (__bridge id)kSecAttrAccount: account,
+        (__bridge id)kSecValueData: data
+    };
+
+    SecItemAdd((__bridge CFDictionaryRef)addQuery, NULL);
+
+    return newID;
+}
+
++ (NSString *)createRawDeviceID {
+
+    UIDevice *device = [UIDevice currentDevice];
+
+    NSString *uuid = [[device identifierForVendor] UUIDString];
+    NSString *model = device.model;
+    NSString *system = device.systemVersion;
+
+    NSString *raw = [NSString stringWithFormat:@"%@-%@-%@", uuid, model, system];
+
+    const char *cStr = [raw UTF8String];
+
+    unsigned char digest[CC_SHA256_DIGEST_LENGTH];
+    CC_SHA256(cStr, (CC_LONG)strlen(cStr), digest);
+
+    NSMutableString *hash = [NSMutableString string];
+
+    for (int i = 0; i < CC_SHA256_DIGEST_LENGTH; i++) {
+        [hash appendFormat:@"%02x", digest[i]];
+    }
+
+    return hash;
+}
+
++ (NSString *)generate {
+
+    // 🔥 أهم فرق: ثابت حتى بعد حذف التطبيق
+    return [self getOrCreateKeychainID];
 }
 
 @end
