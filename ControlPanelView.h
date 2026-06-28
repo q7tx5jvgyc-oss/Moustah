2813b71ae788d5721447d9c0a0b7d267a79b8799
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-28 14:50:47 +0300

    Update ControlPanelView.h

--- ControlPanelView.h
+++ ControlPanelView.h
@@ -1,4 +1,19 @@
 #import <UIKit/UIKit.h>
 
 @interface ControlPanelView : UIView
+
+// UI Core
+@property (nonatomic, strong) UIView *panel;
+
+// Macro System
+@property (nonatomic, strong) NSMutableArray *macros;
+@property (nonatomic, strong) NSMutableArray *targets;
+
+// State
+@property (nonatomic, assign) BOOL recording;
+@property (nonatomic, strong) NSString *macroName;
+
+// Engine
+@property (nonatomic, strong) NSTimer *engine;
+
 @end
