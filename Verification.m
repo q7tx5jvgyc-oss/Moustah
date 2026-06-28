8391e7014f72e48febd74086cc4d54d5ce1f4a07
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-29 00:01:25 +0300

    Update Verification.m

--- Verification.m
+++ Verification.m
@@ -1,12 +1,10 @@
 #import "Verification.h"
 #import <UIKit/UIKit.h>
 
-static NSString *const kMostashAuthCode = @"MOSTAH77669";
 static NSString *const kIsMostashVerifiedKey = @"isMostashVerified";
 
 @interface MostashVerification () <UITextFieldDelegate>
 
-@property (nonatomic, strong) UIWindow *verificationWindow;
 @property (nonatomic, strong) UIView *overlayView;
 @property (nonatomic, strong) UITextField *codeTextField;
 @property (nonatomic, assign) BOOL verified;
@@ -23,69 +21,45 @@ static NSString *const kIsMostashVerifiedKey = @"isMostashVerified";
     return self;
 }
 
-- (BOOL)isVerified {
-    return self.verified;
-}
-
 #pragma mark - Show
 
 - (void)showVerificationIfNeeded {
-
-    if (self.isVerified) return;
+    if (self.verified) return;
 
     dispatch_async(dispatch_get_main_queue(), ^{
         [self buildOverlay];
     });
 }
 
-#pragma mark - Build UI (ULTRA FIX)
+#pragma mark - UI
 
 - (void)buildOverlay {
 
-    // 💀 نجيب أعلى window حقيقي (مهم للألعاب)
-    UIWindow *baseWindow = nil;
+    UIWindow *baseWindow = UIApplication.sharedApplication.keyWindow;
 
-    for (UIWindow *w in [UIApplication sharedApplication].windows) {
-        if (w.isKeyWindow) {
-            baseWindow = w;
-            break;
-        }
-    }
-
-    if (!baseWindow) {
-        baseWindow = [UIApplication sharedApplication].windows.firstObject;
-    }
-
-    // 💀 Overlay يغطي اللعبة
     self.overlayView = [[UIView alloc] initWithFrame:baseWindow.bounds];
     self.overlayView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75];
-    self.overlayView.tag = 99999;
 
-    // Container
-    UIView *container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 300, 220)];
+    UIView *container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 260)];
     container.center = self.overlayView.center;
     container.backgroundColor = UIColor.whiteColor;
     container.layer.cornerRadius = 12;
 
-    // Title
-    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(10, 20, 280, 30)];
-    title.text = @"نظام حماية موستاش";
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(10, 15, 300, 30)];
+    title.text = @"MOSTASH VERIFICATION";
     title.textAlignment = NSTextAlignmentCenter;
     title.font = [UIFont boldSystemFontOfSize:18];
     [container addSubview:title];
 
-    // Input
-    self.codeTextField = [[UITextField alloc] initWithFrame:CGRectMake(30, 90, 240, 35)];
-    self.codeTextField.placeholder = @"أدخل الكود";
+    self.codeTextField = [[UITextField alloc] initWithFrame:CGRectMake(20, 80, 280, 40)];
+    self.codeTextField.placeholder = @"Enter Code";
     self.codeTextField.textAlignment = NSTextAlignmentCenter;
     self.codeTextField.borderStyle = UITextBorderStyleRoundedRect;
-    self.codeTextField.delegate = self;
     [container addSubview:self.codeTextField];
 
-    // Button
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    btn.frame = CGRectMake(30, 140, 240, 40);
-    [btn setTitle:@"تفعيل" forState:UIControlStateNormal];
+    btn.frame = CGRectMake(20, 140, 280, 45);
+    [btn setTitle:@"VERIFY" forState:UIControlStateNormal];
     btn.backgroundColor = UIColor.systemGreenColor;
     btn.tintColor = UIColor.whiteColor;
     btn.layer.cornerRadius = 8;
@@ -93,21 +67,67 @@ static NSString *const kIsMostashVerifiedKey = @"isMostashVerified";
     [container addSubview:btn];
 
     [self.overlayView addSubview:container];
-
-    // 💀 نضيف فوق نافذة اللعبة مباشرة
     [baseWindow addSubview:self.overlayView];
-
-    // 💀 نجبرها تظهر فوق كل شيء
-    [baseWindow bringSubviewToFront:self.overlayView];
 }
 
-#pragma mark - Check Code
+#pragma mark - CODES CHECK (35 CODES)
 
 - (void)checkCode {
 
     NSString *entered = self.codeTextField.text;
 
-    if ([entered isEqualToString:kMostashAuthCode]) {
+    if (entered.length == 0) return;
+
+    // ⏳ DAILY CODES (15)
+    NSArray *dailyCodes = @[
+        @"MOSTASH7A9K",
+        @"MOSTASH2B6M",
+        @"MOSTASH8C1V",
+        @"MOSTASH5D7N",
+        @"MOSTASH9E3X",
+        @"MOSTASH1F8T",
+        @"MOSTASH6G2R",
+        @"MOSTASH3H9Q",
+        @"MOSTASH4J5K",
+        @"MOSTASH7L1P",
+        @"MOSTASH2M8V",
+        @"MOSTASH9N3Y",
+        @"MOSTASH5P6X",
+        @"MOSTASH1Q7B",
+        @"MOSTASH8R2T"
+    ];
+
+    // 💎 PERMANENT CODES (20)
+    NSArray *permanentCodes = @[
+        @"MOSTASH7A9K1XQ3",
+        @"MOSTASH2B6M9LZ8",
+        @"MOSTASH8C1V4RT5",
+        @"MOSTASH5D7N2PQ9",
+        @"MOSTASH9E3X6KM1",
+        @"MOSTASH1F8T7YV4",
+        @"MOSTASH6G2R9LX7",
+        @"MOSTASH3H9Q1BZ6",
+        @"MOSTASH4J5K8NM2",
+        @"MOSTASH7L1P3XT9",
+        @"MOSTASH2M8V6QK4",
+        @"MOSTASH9N3Y1RT7",
+        @"MOSTASH5P6X8LM2",
+        @"MOSTASH1Q7B4NV9",
+        @"MOSTASH8R2T6YK5",
+        @"MOSTASH3S9L1PX7",
+        @"MOSTASH6T4M8QZ2",
+        @"MOSTASH2V1K9RL6",
+        @"MOSTASH7W8X3MN4",
+        @"MOSTASH9Y5Q2TB1"
+    ];
+
+    BOOL isValid = NO;
+
+    if ([dailyCodes containsObject:entered] || [permanentCodes containsObject:entered]) {
+        isValid = YES;
+    }
+
+    if (isValid) {
 
         self.verified = YES;
 
@@ -116,21 +136,19 @@ static NSString *const kIsMostashVerifiedKey = @"isMostashVerified";
 
         [self.overlayView removeFromSuperview];
 
-        NSLog(@"✔ VERIFIED SUCCESS");
-
+        NSLog(@"✔ VIP VERIFIED SUCCESS");
     } else {
-
         [self showError];
     }
 }
 
-#pragma mark - Error
+#pragma mark - ERROR
 
 - (void)showError {
 
     UIAlertController *alert =
-    [UIAlertController alertControllerWithTitle:@"خطأ"
-                                        message:@"الكود غير صحيح"
+    [UIAlertController alertControllerWithTitle:@"ERROR"
+                                        message:@"Invalid Code"
                                  preferredStyle:UIAlertControllerStyleAlert];
 
     UIAlertAction *ok =
@@ -140,16 +158,8 @@ static NSString *const kIsMostashVerifiedKey = @"isMostashVerified";
 
     [alert addAction:ok];
 
-    UIViewController *topVC = [UIApplication sharedApplication].keyWindow.rootViewController;
+    UIViewController *topVC = UIApplication.sharedApplication.keyWindow.rootViewController;
     [topVC presentViewController:alert animated:YES completion:nil];
-}
-
-#pragma mark - TextField
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    [textField resignFirstResponder];
-    [self checkCode];
-    return YES;
 }
 
 @end
