ce007fb4b314c4960ba9037ca8d917f27ea17f05
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-28 14:29:21 +0300

    Update FloatingButton.m

--- FloatingButton.m
+++ FloatingButton.m
@@ -21,14 +21,31 @@
 
     self.button = [UIButton buttonWithType:UIButtonTypeCustom];
     self.button.frame = self.bounds;
+
+    // شكل دائري
     self.button.backgroundColor = UIColor.systemBlueColor;
-    self.button.layer.cornerRadius = 35;
-    [self.button setTitle:@"M" forState:UIControlStateNormal];
+    self.button.layer.cornerRadius = self.bounds.size.width / 2;
+    self.button.clipsToBounds = YES;
+
+    // تحميل الصورة من الإنترنت
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        NSURL *url = [NSURL URLWithString:@"https://l.top4top.io/p_3831x8fzt0.jpeg"];
+        NSData *data = [NSData dataWithContentsOfURL:url];
+        UIImage *image = [UIImage imageWithData:data];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (image) {
+                [self.button setImage:image forState:UIControlStateNormal];
+                self.button.imageView.contentMode = UIViewContentModeScaleAspectFit;
+            }
+        });
+    });
 
     [self.button addTarget:self action:@selector(tap) forControlEvents:UIControlEventTouchUpInside];
 
     [self addSubview:self.button];
 
+    // سحب الزر
     UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(drag:)];
     [self addGestureRecognizer:pan];
 }
