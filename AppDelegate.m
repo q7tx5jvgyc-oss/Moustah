f583aa9e8be0e0990d4247fca740f467a54477bc
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-28 15:30:49 +0300

    Update AppDelegate.m

--- AppDelegate.m
+++ AppDelegate.m
@@ -1,5 +1,6 @@
 #import "AppDelegate.h"
 #import "ConfigManager.h"
+#import "ActivationViewController.h"
 
 @implementation AppDelegate
 
@@ -8,6 +9,27 @@
     NSLog(@"App Started");
 
     [[ConfigManager shared] loadConfig];
+
+    UIWindow *window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window = window;
+
+    // 🔐 تحقق من التفعيل
+    if ([[ConfigManager shared] isActivated]) {
+
+        // التطبيق مفعل → يفتح مباشرة (هنا ضع الـ main VC)
+        UIViewController *mainVC = [UIViewController new];
+        mainVC.view.backgroundColor = UIColor.whiteColor;
+
+        window.rootViewController = mainVC;
+
+    } else {
+
+        // غير مفعل → يروح لشاشة التفعيل
+        ActivationViewController *vc = [ActivationViewController new];
+        window.rootViewController = vc;
+    }
+
+    [window makeKeyAndVisible];
 
     return YES;
 }
