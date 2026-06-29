2015f7083616c6653acfbedabd8c80530a0a9a13
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-30 02:14:00 +0300

    Update ActivationViewController.m

--- ActivationViewController.m
+++ ActivationViewController.m
@@ -1,119 +1,34 @@
-#import "VerificationViewController.h"
+#import "ActivationViewController.h"
+#import "LicenseManager.h"
 
-@interface VerificationViewController ()
-
-@property (nonatomic, strong) UITextField *codeField;
-@property (nonatomic, strong) UIView *card;
-
-@end
-
-@implementation VerificationViewController
+@implementation ActivationViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-    [self setupUI];
-}
+    self.view.backgroundColor = UIColor.blackColor;
 
-#pragma mark - UI
-
-- (void)setupUI {
-
-    self.view.backgroundColor = [UIColor blackColor];
-
-    UIView *overlay = [[UIView alloc] initWithFrame:self.view.bounds];
-    overlay.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.96];
-    overlay.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    [self.view addSubview:overlay];
-
-    self.card = [[UIView alloc] initWithFrame:CGRectMake(40, 160, self.view.frame.size.width - 80, 380)];
-    self.card.backgroundColor = [[UIColor darkGrayColor] colorWithAlphaComponent:0.85];
-    self.card.layer.cornerRadius = 18;
-
-    [overlay addSubview:self.card];
-
-    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, self.card.frame.size.width, 40)];
-    title.text = @"MOSTASH VIP";
-    title.textAlignment = NSTextAlignmentCenter;
-    title.textColor = UIColor.whiteColor;
-    title.font = [UIFont boldSystemFontOfSize:28];
-    [self.card addSubview:title];
-
-    self.codeField = [[UITextField alloc] initWithFrame:CGRectMake(20, 110, self.card.frame.size.width - 40, 50)];
-    self.codeField.placeholder = @"Enter Code";
-    self.codeField.textAlignment = NSTextAlignmentCenter;
-    self.codeField.backgroundColor = UIColor.whiteColor;
-    self.codeField.layer.cornerRadius = 10;
-    [self.card addSubview:self.codeField];
+    UITextField *field = [[UITextField alloc] initWithFrame:CGRectMake(50, 200, 250, 40)];
+    field.placeholder = @"Enter Code";
+    field.backgroundColor = UIColor.whiteColor;
 
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-    btn.frame = CGRectMake(20, 190, self.card.frame.size.width - 40, 55);
-    [btn setTitle:@"VERIFY" forState:UIControlStateNormal];
-    btn.backgroundColor = UIColor.systemGreenColor;
-    [btn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
-    btn.layer.cornerRadius = 10;
-    [btn addTarget:self action:@selector(checkCode) forControlEvents:UIControlEventTouchUpInside];
-    [self.card addSubview:btn];
+    btn.frame = CGRectMake(50, 260, 250, 40);
+    [btn setTitle:@"Activate" forState:UIControlStateNormal];
+
+    [btn addTarget:self action:@selector(check:)
+  forControlEvents:UIControlEventTouchUpInside];
+
+    [self.view addSubview:field];
+    [self.view addSubview:btn];
 }
 
-#pragma mark - SERVER REQUEST
+- (void)check:(UIButton *)btn {
 
-- (void)checkCode {
+    [[NSUserDefaults standardUserDefaults] setObject:@"VALID_CODE"
+                                              forKey:@"license"];
 
-    NSString *code = self.codeField.text;
-    if (code.length == 0) return;
-
-    NSString *deviceID = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
-
-    NSURL *url = [NSURL URLWithString:@"https://YOUR-SERVER.com/api/verify"];
-
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
-    request.HTTPMethod = @"POST";
-    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-
-    NSDictionary *body = @{
-        @"code": code,
-        @"device": deviceID
-    };
-
-    NSData *data = [NSJSONSerialization dataWithJSONObject:body options:0 error:nil];
-    request.HTTPBody = data;
-
-    NSURLSessionDataTask *task =
-    [[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
-
-        if (error || !data) return;
-
-        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-
-        dispatch_async(dispatch_get_main_queue(), ^{
-
-            if ([json[@"status"] isEqualToString:@"success"]) {
-
-                [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"MOSTASH_VIP"];
-                [[NSUserDefaults standardUserDefaults] synchronize];
-
-                [self showAlert:@"WELCOME VIP 🔥" message:json[@"message"]];
-
-            } else {
-                [self showAlert:@"FAILED ❌" message:json[@"message"]];
-            }
-        });
-
-    }];
-
-    [task resume];
-}
-
-- (void)showAlert:(NSString *)title message:(NSString *)msg {
-
-    UIAlertController *alert =
-    [UIAlertController alertControllerWithTitle:title message:msg preferredStyle:UIAlertControllerStyleAlert];
-
-    UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
-    [alert addAction:ok];
-
-    [self presentViewController:alert animated:YES completion:nil];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
