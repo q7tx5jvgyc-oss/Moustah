a6ff9b221ca9717c2a01f7a671b2471e2b5e7f2c
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-29 19:27:53 +0300

    Update Verification.m

--- Verification.m
+++ Verification.m
@@ -1,142 +1,179 @@
 #import "Verification.h"
 #import <UIKit/UIKit.h>
 
-static NSString *const kIsMostashVerifiedKey = @"isMostashVerified";
+static NSString *const kVIPKey = @"MOSTASH_VIP";
+static NSString *const kCodesKey = @"MOSTASH_CODES_DB";
 
-@interface MostashVerification () <UITextFieldDelegate>
-
-@property (nonatomic, strong) UIView *overlayView;
-@property (nonatomic, strong) UITextField *codeTextField;
-@property (nonatomic, assign) BOOL verified;
-
+@interface MostashVerification ()
+@property (nonatomic, strong) UIWindow *overlayWindow;
+@property (nonatomic, strong) UITextField *codeField;
 @end
 
 @implementation MostashVerification
 
-- (instancetype)init {
-    self = [super init];
-    if (self) {
-        _verified = [[NSUserDefaults standardUserDefaults] boolForKey:kIsMostashVerifiedKey];
-    }
-    return self;
+#pragma mark - INIT DEFAULT CODES
+
+- (NSArray *)loadDefaultCodes {
+
+    return @[
+        // 🔵 DAILY CODES (15)
+        @{@"code": @"MOSTASH7A9K", @"type": @"daily", @"used": @NO},
+        @{@"code": @"MOSTASH2B6M", @"type": @"daily", @"used": @NO},
+        @{@"code": @"MOSTASH8C1V", @"type": @"daily", @"used": @NO},
+        @{@"code": @"MOSTASH5D7N", @"type": @"daily", @"used": @NO},
+        @{@"code": @"MOSTASH9E3X", @"type": @"daily", @"used": @NO},
+        @{@"code": @"MOSTASH1F8T", @"type": @"daily", @"used": @NO},
+        @{@"code": @"MOSTASH6G2R", @"type": @"daily", @"used": @NO},
+        @{@"code": @"MOSTASH3H9Q", @"type": @"daily", @"used": @NO},
+        @{@"code": @"MOSTASH4J5K", @"type": @"daily", @"used": @NO},
+        @{@"code": @"MOSTASH7L1P", @"type": @"daily", @"used": @NO},
+
+        // 🟡 PERMANENT CODES (20)
+        @{@"code": @"MOSTASH7A9K1XQ3", @"type": @"permanent", @"used": @NO},
+        @{@"code": @"MOSTASH2B6M9LZ8", @"type": @"permanent", @"used": @NO},
+        @{@"code": @"MOSTASH8C1V4RT5", @"type": @"permanent", @"used": @NO},
+        @{@"code": @"MOSTASH5D7N2PQ9", @"type": @"permanent", @"used": @NO},
+        @{@"code": @"MOSTASH9E3X6KM1", @"type": @"permanent", @"used": @NO},
+        @{@"code": @"MOSTASH1F8T7YV4", @"type": @"permanent", @"used": @NO},
+        @{@"code": @"MOSTASH6G2R9LX7", @"type": @"permanent", @"used": @NO},
+        @{@"code": @"MOSTASH3H9Q1BZ6", @"type": @"permanent", @"used": @NO},
+        @{@"code": @"MOSTASH4J5K8NM2", @"type": @"permanent", @"used": @NO},
+        @{@"code": @"MOSTASH7L1P3XT9", @"type": @"permanent", @"used": @NO}
+    ];
 }
 
-#pragma mark - Show
+#pragma mark - LOAD DB
+
+- (NSMutableArray *)loadDB {
+
+    NSMutableArray *db =
+    [[[NSUserDefaults standardUserDefaults] objectForKey:kCodesKey] mutableCopy];
+
+    if (!db) {
+        db = [[self loadDefaultCodes] mutableCopy];
+        [[NSUserDefaults standardUserDefaults] setObject:db forKey:kCodesKey];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+    }
+
+    return db;
+}
+
+#pragma mark - SHOW UI
 
 - (void)showVerificationIfNeeded {
-    if (self.verified) return;
+
+    if ([[NSUserDefaults standardUserDefaults] boolForKey:kVIPKey]) return;
 
     dispatch_async(dispatch_get_main_queue(), ^{
-        [self buildOverlay];
+        [self buildUI];
     });
 }
 
-#pragma mark - UI
+- (UIWindow *)getWindow {
 
-- (void)buildOverlay {
+    UIWindow *w = nil;
 
-    UIWindow *baseWindow = UIApplication.sharedApplication.keyWindow;
+    if (@available(iOS 13.0, *)) {
+        for (UIScene *scene in UIApplication.sharedApplication.connectedScenes) {
+            if (scene.activationState == UISceneActivationStateForegroundActive &&
+                [scene isKindOfClass:[UIWindowScene class]]) {
 
-    self.overlayView = [[UIView alloc] initWithFrame:baseWindow.bounds];
-    self.overlayView.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.75];
+                for (UIWindow *win in ((UIWindowScene *)scene).windows) {
+                    if (win.isKeyWindow) {
+                        w = win;
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
-    UIView *container = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 260)];
-    container.center = self.overlayView.center;
-    container.backgroundColor = UIColor.whiteColor;
-    container.layer.cornerRadius = 12;
+    if (!w) w = UIApplication.sharedApplication.windows.firstObject;
 
-    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(10, 15, 300, 30)];
-    title.text = @"MOSTASH VERIFICATION";
+    return w;
+}
+
+- (void)buildUI {
+
+    UIWindow *base = [self getWindow];
+
+    self.overlayWindow = [[UIWindow alloc] initWithFrame:base.bounds];
+    self.overlayWindow.windowLevel = UIWindowLevelAlert + 999;
+    self.overlayWindow.hidden = NO;
+
+    UIViewController *vc = [UIViewController new];
+    vc.view.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.85];
+
+    UIView *card = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 260)];
+    card.center = vc.view.center;
+    card.backgroundColor = UIColor.whiteColor;
+    card.layer.cornerRadius = 14;
+
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 15, 320, 30)];
+    title.text = @"MOSTASH VIP SYSTEM";
     title.textAlignment = NSTextAlignmentCenter;
     title.font = [UIFont boldSystemFontOfSize:18];
-    [container addSubview:title];
 
-    self.codeTextField = [[UITextField alloc] initWithFrame:CGRectMake(20, 80, 280, 40)];
-    self.codeTextField.placeholder = @"Enter Code";
-    self.codeTextField.textAlignment = NSTextAlignmentCenter;
-    self.codeTextField.borderStyle = UITextBorderStyleRoundedRect;
-    [container addSubview:self.codeTextField];
+    self.codeField = [[UITextField alloc] initWithFrame:CGRectMake(20, 80, 280, 40)];
+    self.codeField.placeholder = @"Enter Code";
+    self.codeField.textAlignment = NSTextAlignmentCenter;
+    self.codeField.borderStyle = UITextBorderStyleRoundedRect;
 
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
     btn.frame = CGRectMake(20, 140, 280, 45);
     [btn setTitle:@"VERIFY" forState:UIControlStateNormal];
     btn.backgroundColor = UIColor.systemGreenColor;
-    btn.tintColor = UIColor.whiteColor;
+    [btn setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
     btn.layer.cornerRadius = 8;
     [btn addTarget:self action:@selector(checkCode) forControlEvents:UIControlEventTouchUpInside];
-    [container addSubview:btn];
 
-    [self.overlayView addSubview:container];
-    [baseWindow addSubview:self.overlayView];
+    [card addSubview:title];
+    [card addSubview:self.codeField];
+    [card addSubview:btn];
+    [vc.view addSubview:card];
+
+    self.overlayWindow.rootViewController = vc;
+    [self.overlayWindow makeKeyAndVisible];
 }
 
-#pragma mark - CODES CHECK (35 CODES)
+#pragma mark - CHECK ENGINE
 
 - (void)checkCode {
 
-    NSString *entered = self.codeTextField.text;
+    NSString *input = self.codeField.text;
+    if (input.length == 0) return;
 
-    if (entered.length == 0) return;
+    NSMutableArray *db = [self loadDB];
 
-    // ⏳ DAILY CODES (15)
-    NSArray *dailyCodes = @[
-        @"MOSTASH7A9K",
-        @"MOSTASH2B6M",
-        @"MOSTASH8C1V",
-        @"MOSTASH5D7N",
-        @"MOSTASH9E3X",
-        @"MOSTASH1F8T",
-        @"MOSTASH6G2R",
-        @"MOSTASH3H9Q",
-        @"MOSTASH4J5K",
-        @"MOSTASH7L1P",
-        @"MOSTASH2M8V",
-        @"MOSTASH9N3Y",
-        @"MOSTASH5P6X",
-        @"MOSTASH1Q7B",
-        @"MOSTASH8R2T"
-    ];
+    __block BOOL found = NO;
 
-    // 💎 PERMANENT CODES (20)
-    NSArray *permanentCodes = @[
-        @"MOSTASH7A9K1XQ3",
-        @"MOSTASH2B6M9LZ8",
-        @"MOSTASH8C1V4RT5",
-        @"MOSTASH5D7N2PQ9",
-        @"MOSTASH9E3X6KM1",
-        @"MOSTASH1F8T7YV4",
-        @"MOSTASH6G2R9LX7",
-        @"MOSTASH3H9Q1BZ6",
-        @"MOSTASH4J5K8NM2",
-        @"MOSTASH7L1P3XT9",
-        @"MOSTASH2M8V6QK4",
-        @"MOSTASH9N3Y1RT7",
-        @"MOSTASH5P6X8LM2",
-        @"MOSTASH1Q7B4NV9",
-        @"MOSTASH8R2T6YK5",
-        @"MOSTASH3S9L1PX7",
-        @"MOSTASH6T4M8QZ2",
-        @"MOSTASH2V1K9RL6",
-        @"MOSTASH7W8X3MN4",
-        @"MOSTASH9Y5Q2TB1"
-    ];
+    for (NSMutableDictionary *item in db) {
 
-    BOOL isValid = NO;
+        if ([item[@"code"] isEqualToString:input]) {
 
-    if ([dailyCodes containsObject:entered] || [permanentCodes containsObject:entered]) {
-        isValid = YES;
+            if ([item[@"used"] boolValue] &&
+                [item[@"type"] isEqualToString:@"daily"]) {
+                return;
+            }
+
+            item[@"used"] = @YES;
+
+            found = YES;
+            break;
+        }
     }
 
-    if (isValid) {
+    if (found) {
 
-        self.verified = YES;
-
-        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kIsMostashVerifiedKey];
+        [[NSUserDefaults standardUserDefaults] setBool:YES forKey:kVIPKey];
+        [[NSUserDefaults standardUserDefaults] setObject:db forKey:kCodesKey];
         [[NSUserDefaults standardUserDefaults] synchronize];
 
-        [self.overlayView removeFromSuperview];
+        [self.overlayWindow removeFromSuperview];
+        self.overlayWindow = nil;
 
-        NSLog(@"✔ VIP VERIFIED SUCCESS");
+        NSLog(@"✔ VIP VERIFIED");
+
     } else {
         [self showError];
     }
@@ -148,18 +185,15 @@ static NSString *const kIsMostashVerifiedKey = @"isMostashVerified";
 
     UIAlertController *alert =
     [UIAlertController alertControllerWithTitle:@"ERROR"
-                                        message:@"Invalid Code"
+                                        message:@"Invalid or Used Code"
                                  preferredStyle:UIAlertControllerStyleAlert];
 
-    UIAlertAction *ok =
-    [UIAlertAction actionWithTitle:@"OK"
-                             style:UIAlertActionStyleDefault
-                           handler:nil];
+    [alert addAction:[UIAlertAction actionWithTitle:@"OK"
+                                              style:UIAlertActionStyleDefault
+                                            handler:nil]];
 
-    [alert addAction:ok];
-
-    UIViewController *topVC = UIApplication.sharedApplication.keyWindow.rootViewController;
-    [topVC presentViewController:alert animated:YES completion:nil];
+    UIViewController *vc = self.overlayWindow.rootViewController;
+    [vc presentViewController:alert animated:YES completion:nil];
 }
 
 @end
