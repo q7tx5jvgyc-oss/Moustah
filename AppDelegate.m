820963383e548d579be55831a76919e3dc0b19e6
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-29 19:21:22 +0300

    Update AppDelegate.m

--- AppDelegate.m
+++ AppDelegate.m
@@ -8,21 +8,53 @@
 
     [[ConfigManager shared] loadConfig];
 
-    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window = [[UIWindow alloc] initWithFrame:UIScreen.mainScreen.bounds];
 
-    UIViewController *root;
+    UIViewController *rootVC = [self initialController];
 
-    if ([[ConfigManager shared] isActivated]) {
-        root = [UIViewController new];
-        root.view.backgroundColor = UIColor.systemBackgroundColor;
-    } else {
-        root = [ActivationViewController new];
-    }
-
-    self.window.rootViewController = root;
+    self.window.rootViewController = rootVC;
     [self.window makeKeyAndVisible];
 
     return YES;
+}
+
+#pragma mark - Router
+
+- (UIViewController *)initialController {
+
+    BOOL activated = [[ConfigManager shared] isActivated];
+
+    if (activated) {
+
+        UIViewController *mainVC = [UIViewController new];
+        mainVC.view.backgroundColor = UIColor.systemBackgroundColor;
+
+        return mainVC;
+    }
+
+    return [self activationController];
+}
+
+- (UIViewController *)activationController {
+
+    ActivationViewController *vc = [ActivationViewController new];
+    vc.view.backgroundColor = UIColor.systemBackgroundColor;
+
+    return vc;
+}
+
+#pragma mark - State Refresh (مهم)
+
+- (void)refreshRoot {
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+
+        UIViewController *newRoot = [self initialController];
+
+        self.window.rootViewController = newRoot;
+
+        [self.window makeKeyAndVisible];
+    });
 }
 
 @end
