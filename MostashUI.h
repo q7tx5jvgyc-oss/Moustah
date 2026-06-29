f098b5ff3f9a7a1dd001ff8d78117848b8ede87c
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-29 19:23:19 +0300

    Update MostashUI.h

--- MostashUI.h
+++ MostashUI.h
@@ -1,25 +1,8 @@
 #import <UIKit/UIKit.h>
 
-@interface MostashFloatingButton : UIWindow
+@interface MostashFloatingButton : UIView
 
-@property (nonatomic, strong) UIButton *mostashButton;
-@property (nonatomic, strong) UIView *controlPanel;
-@property (nonatomic, strong) UISlider *speedSlider;
-@property (nonatomic, strong) UILabel *speedLabel;
-
-// Main control buttons
-@property (nonatomic, strong) UIButton *startButton;
-@property (nonatomic, strong) UIButton *stopButton;
-@property (nonatomic, strong) UIButton *addTargetButton;
-@property (nonatomic, strong) UIButton *clearTargetsButton;
-
-// Additional features buttons
-@property (nonatomic, strong) UIButton *removeMuteButton;
-@property (nonatomic, strong) UIButton *speedUpGameButton;
-@property (nonatomic, strong) UIButton *vipModeButton;
-@property (nonatomic, strong) UIButton *antiBanButton;
-
-- (void)showFloatingButton;
-- (void)hideFloatingButton;
+- (void)show;
+- (void)hide;
 
 @end
