fdc8533085acb0d7f6f54a86672d63ba17f4f9d8
q7tx5jvgyc-oss <q7tx5jvgyc@privaterelay.appleid.com>
2026-06-30 18:14:38 +0300

    Update FloatingButton.h

--- FloatingButton.h
+++ FloatingButton.h
@@ -1,4 +1,11 @@
 #import <UIKit/UIKit.h>
 
 @interface FloatingButton : UIView
+
+@property (nonatomic, assign) BOOL isMenuVisible; // خاصية تتبع حالة ظهور القائمة
+
++ (instancetype)sharedInstance;
++ (instancetype)shared;
+- (void)setMenuVisible:(BOOL)visible;
+
 @end
